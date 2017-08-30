915bdd11d99a0d65205013fd4c0d61f804a468a8
coreyfedde <coreysfedde@gmail.com>
2017-08-30 18:55:19 -0400

    Creates new component for displaying loan review
    - Has new card component to display the virtuals from the loan object

--- app/models/loan.js
+++ app/models/loan.js
@@ -3,5 +3,10 @@ import DS from 'ember-data';
 export default DS.Model.extend({
   name: DS.attr('string'),
   principal: DS.attr('string'),
-  lender: DS.attr('string')
+  lender: DS.attr('string'),
+  total_amount: DS.attr('string'),
+  total_interest: DS.attr('string'),
+  monthly_payment: DS.attr('string'),
+  interest_rate: DS.attr('string'),
+  loan_length: DS.attr('string')
 });
