ce1efc58fd3d119fe836eeda642c5add7bbe55ce
coreyfedde <coreysfedde@gmail.com>
2017-08-31 15:50:00 -0400

    Name change for deployment

--- app/adapters/application.js
+++ app/adapters/application.js
@@ -1,4 +1,4 @@
-import ENV from 'ga-wdi-boston.ember-auth/config/environment';
+import ENV from 'capstone-front-ember/config/environment';
 import ActiveModelAdapter from 'active-model-adapter';
 
 import Ember from 'ember';
--- config/environment.js
+++ config/environment.js
@@ -3,7 +3,7 @@
 module.exports = function (environment) {
   'use strict';
   const ENV = {
-    modulePrefix: 'ga-wdi-boston.ember-auth',
+    modulePrefix: 'capstone-front-ember',
     environment: environment,
     rootURL: '/',
     locationType: 'auto',
