d68e9f4fd307c31eb756028c7613cf6329929979
coreyfedde <coreysfedde@gmail.com>
2017-08-30 23:26:08 -0400

    Creates component to parent loan view
    - Creates loan-parent to act as a parent component between loan and the
    dropdown component
    - Modified route and component files so that actions on tools-drop-down
    will work

--- app/routes/loan.js
+++ app/routes/loan.js
@@ -4,4 +4,13 @@ export default Ember.Route.extend({
   model (params) {
     return this.get('store').findRecord('loan', params.loan_id)
   },
+  actions: {
+    createLoan(loan) {
+      let newLoan = this.get('store').createRecord('loan', loan)
+      newLoan.save()
+    },
+    deleteLoan(loan) {
+      loan.destroyRecord()
+    }
+  }
 });
