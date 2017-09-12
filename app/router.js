4c403b8370cf4df5b0adb53f0ff8dcedd1d2abe7
coreyfedde <coreysfedde@gmail.com>
2017-09-11 22:58:05 -0400

    Creates routes for review, versus, and balance graphs
    - These routes will only hold one individual graph
    - Will give more information about the data
    - Model will be similar to the one for loan

--- app/router.js
+++ app/router.js
@@ -14,6 +14,9 @@ Router.map(function () {
   this.route('profile');
   this.route('loan', { path: '/loans/:loan_id' });
   this.route('loans-edit', { path: '/loans/:loan_id/edit'});
+  this.route('loan-review', { path: '/loans/:loan_id/review'});
+  this.route('interest-vs-principal', { path: '/loans/:loan_id/versus'});
+  this.route('balance-chart', { path: '/loans/:loan_id/balance'});
 });
 
 export default Router;
---
+++ tests/unit/routes/interest-vs-principal-test.js
@@ -0,0 +1,11 @@
+import { moduleFor, test } from 'ember-qunit';
+
+moduleFor('route:interest-vs-principal', 'Unit | Route | interest vs principal', {
+  // Specify the other units that are required for this test.
+  // needs: ['controller:foo']
+});
+
+test('it exists', function(assert) {
+  let route = this.subject();
+  assert.ok(route);
+});
